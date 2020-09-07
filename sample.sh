8ec0b0fd58c486309df4682d1479ab59335ed58b
rancher123 <40929647+penchalgit@users.noreply.github.com>
2020-09-07 11:43:34 +0200

    Create sample.sh

---
+++ sample.sh
@@ -0,0 +1,11 @@
+import groovy.io.FileType
+
+String folderPath = "/home/ubuntu/java_docker" + "\\" + "Dockerfile"
+def list = []
+def dir = new File("/home/ubuntu/java_docker")
+dir.eachFileRecurse (FileType.FILES) { file ->
+  list << file
+}
+list.each {
+println it.findAll() == "8080" // Just files witch include in list "8080" name
+}
